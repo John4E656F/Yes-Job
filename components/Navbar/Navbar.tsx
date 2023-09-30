d814ae45283dc698944d0ec51fbaad4f7609f351
John4E656F <jm49andres@gmail.com>
2023-09-30 19:57:05 +0200

    Initialize

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
+      <div className='w-full max-w-4xl flex justify-between items-center p-3 text-sm text-foreground'></div>
+    </nav>
+  );
+};
+
+export default Navbar;
