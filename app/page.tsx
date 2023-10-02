503d4ee883e1912d1d30584774d9007af60e58cb
John4E656F <jm49andres@gmail.com>
2023-10-02 15:16:12 +0200

    Build hero

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,24 @@ export default async function Index() {
     <div className='container items-center justify-center'>
       <Navbar />
       <div className='flex flex-col container py-4 md:py-16 '>
-        <div className='flex flex-col md:flex-row justify-between '>
-          <div className='bg-blue-500 w-auto'>Hello World</div>
-          <Image src='/images/svg/hero.svg' alt='Yes Job' className='w-auto max-w-sm h-full object-fill bg-blue-200 rounded-2xl' />
+        <div className='flex flex-col lg:flex-row gap-16 justify-between items-center '>
+          <div className=' w-auto flex flex-col gap-4'>
+            <h1 className='text-4xl font-semibold'>Trouver les meilleures offres d’emploi dans horeca</h1>
+            <h2 className='text-sm'>
+              Que vous soyez à la recherche d'un emploi à temps plein, que vous souhaitiez gagner un peu plus d'argent en étudiant ou que vous ayez
+              besoin d'argent supplémentaire pour vos vacances - vous trouverez chez nous une offre adaptée à vos besoins !
+            </h2>
+            <button
+              data-collapse-toggle='navbar-default'
+              type='button'
+              className=' md:block md:w-auto items-center px-4 h-11 justify-center text-sm bg-brand-primary text-white rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-gray-200  '
+              aria-controls='navbar-default'
+              aria-expanded='false'
+            >
+              Publier une annonce
+            </button>
+          </div>
+          <Image src='/images/svg/hero.svg' alt='Yes Job' className='w-auto lg:max-w-sm max-h-lg object-fill bg-blue-200 rounded-2xl' />
         </div>
       </div>
     </div>
