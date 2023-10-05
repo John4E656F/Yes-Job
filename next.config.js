5ba534e319fc9ab83d7e883a0ab04420fac3a4c8
John4E656F <jm49andres@gmail.com>
2023-10-05 18:57:00 +0200

    Finish Form Page and Simple Listing Fetch, And Listing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['pykxxycnouynvrmhbbvs.supabase.co'],
+    unoptimized: true,
+  },
+};
 
 module.exports = nextConfig;
