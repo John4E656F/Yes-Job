755df69b67d96e297edaec5f775d83b27512398b
John4E656F <jm49andres@gmail.com>
2023-10-08 19:40:23 +0200

    Fix Link Scroll to the top

--- app/annonce/page.tsx
+++ app/annonce/page.tsx
@@ -11,25 +11,25 @@ const AnnoncePage = () => {
           <h3 className='text-2xl'>YES Job est le meilleur endroit pour trouver et publier des offres d'emploi dans l'horeca</h3>
           <div className='flex md:justify-center'>
             <ul className='flex flex-col text-lg text-left gap-3'>
-              <li className='flex items-center gap-2 overflow-hidden'>
+              <li className='flex items-center gap-2'>
                 <div>
                   <BsPinAngle size={24} />
                 </div>
                 <p>30 jours de promotion sur la page d'accueil</p>
               </li>
-              <li className='flex items-center gap-2  overflow-hidden'>
+              <li className='flex items-center gap-2  '>
                 <div>
                   <BsBullseye size={24} />
                 </div>
                 <p>Optimisation de l'offre</p>
               </li>
-              <li className='flex items-center gap-2  overflow-hidden'>
+              <li className='flex items-center gap-2  '>
                 <div>
                   <BsFacebook size={24} />
                 </div>
                 <p>Campagne Facebook</p>
               </li>
-              <li className='flex items-center gap-2 overflow-hidden'>
+              <li className='flex items-center gap-2 '>
                 <div>
                   <BsGoogle size={24} />
                 </div>
