b360cf815a015f62c8154930ce64627c9cdd78f7
John4E656F <jm49andres@gmail.com>
2023-10-12 18:43:00 +0200

    Full i18n implementation

--- app/[locale]/annonce/page.tsx
+++ app/[locale]/annonce/page.tsx
@@ -1,45 +1,48 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
+import { useTranslations } from 'next-intl';
 import { BsPinAngle, BsBullseye, BsFacebook, BsGoogle } from 'react-icons/bs';
 
 const AnnoncePage = () => {
+  const t = useTranslations('app');
   return (
     <header className='w-full flex justify-center'>
       <div className='flex flex-col container justify-center  py-4 md:py-16 '>
         <div className=' w-auto flex flex-col gap-12 text-center'>
-          <h1 className='text-4xl font-semibold'>Nous sommes heureux que vous souhaitiez ajouter une annonce chez nous !</h1>
-          <h3 className='text-2xl'>YES Job est le meilleur endroit pour trouver et publier des offres d'emploi dans l'horeca</h3>
+          <h1 className='text-4xl font-semibold'>{t('ads.title')}</h1>
+          <h3 className='text-2xl'>{t('ads.subTitle')}</h3>
           <div className='flex md:justify-center'>
             <ul className='flex flex-col text-lg text-left gap-3'>
               <li className='flex items-center gap-2'>
                 <div>
                   <BsPinAngle size={24} />
                 </div>
-                <p>30 jours de promotion sur la page d'accueil</p>
+                <p>{t('ads.li')}</p>
               </li>
               <li className='flex items-center gap-2  '>
                 <div>
                   <BsBullseye size={24} />
                 </div>
-                <p>Optimisation de l'offre</p>
+                <p>{t('ads.li1')}</p>
               </li>
               <li className='flex items-center gap-2  '>
                 <div>
                   <BsFacebook size={24} />
                 </div>
-                <p>Campagne Facebook</p>
+                <p>{t('ads.li2')}</p>
               </li>
               <li className='flex items-center gap-2 '>
                 <div>
                   <BsGoogle size={24} />
                 </div>
-                <p>Campagne Google</p>
+                <p>{t('ads.li3')}</p>
               </li>
               <li className='flex items-center gap-2'>
                 <div>
                   <BsGoogle size={24} />
                 </div>
-                <p>Distribution dans le réseau de recrutement Google for Jobs</p>
+                <p>{t('ads.li4')}</p>
               </li>
             </ul>
           </div>
@@ -48,7 +51,7 @@ const AnnoncePage = () => {
               type='button'
               className='w-full md:block md:w-auto items-center px-4 h-11 justify-center text-sm bg-brand-primary text-white rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-gray-200  '
             >
-              Ajoutez votre annonce gratuitement
+              {t('cta.add')}
             </button>
           </Link>
         </div>
