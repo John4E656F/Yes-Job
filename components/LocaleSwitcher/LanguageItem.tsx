8a0bdfa281d8363e6813b3e063905856427fcefc
John4E656F <jm49andres@gmail.com>
2023-10-13 11:44:04 +0200

    Improve Accessibility

--- components/LocaleSwitcher/LanguageItem.tsx
+++ components/LocaleSwitcher/LanguageItem.tsx
@@ -8,11 +8,9 @@ interface LanguageItemProps {
 export function LanguageItem({ country, isSelected, name, onClick }: LanguageItemProps) {
   return (
     <li
-      role="select"
+      role='listitem'
       className={`${
-        isSelected
-          ? `border-brand-primary shadow-brand-shadow-selected`
-          : `border-current shadow-brand-shadow`
+        isSelected ? `border-brand-primary shadow-brand-shadow-selected` : `border-current shadow-brand-shadow`
       } border-2  px-3 py-3 visited:bg-brand-bg hover:bg-brand-bg focus:bg-brand-bg`}
       onClick={onClick}
     >
