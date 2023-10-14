81c39c01fef8761e10bc0450788fb81e48ba57e7
John4E656F <jm49andres@gmail.com>
2023-10-14 13:29:44 +0200

    Update Navbar for authenticated user, profileMenu

---
+++ types/CompanyTypes.ts
@@ -0,0 +1,9 @@
+export interface CompanyTypes {
+  company_email: string;
+  companyLogo: string;
+  companyName: string;
+  companyTotalRequestCount: number | undefined;
+  created_at: string;
+  id: string;
+  user_id: string | null;
+}
--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,5 @@ export type { PropsWithChildrenOptional, PropsWithChildrenRequired } from './Pro
 export { LanguageCode, type Language, languages } from './LanguageCode';
 export type { FormData } from './FormData';
 export type { TranslationProps } from './TranslationProps';
+export type { CompanyTypes } from './CompanyTypes';
+export type { Database } from './public';
