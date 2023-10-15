79f7cca867584e7b1e2fde1eb6e6677052941e65
John4E656F <jm49andres@gmail.com>
2023-10-15 17:46:52 +0200

    Build Toast

--- types/index.ts
+++ types/index.ts
@@ -4,3 +4,4 @@ export type { FormData } from './FormData';
 export type { TranslationProps } from './TranslationProps';
 export type { UsersTypes } from './UsersTypes';
 export type { Database } from './public';
+export { ToastTitle, type ToastProps } from './ToastProps';
