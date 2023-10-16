ecbb9919a49e444cf9be285ad941ff9eebe4c02d
John4E656F <jm49andres@gmail.com>
2023-10-16 15:26:08 +0200

    Added TimeDifference ago text in the listing card

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,3 +1,4 @@
 export { formatDescription } from './formatDescription';
 export { formatDate } from './formatDate';
 export { generatePassword } from './generatePassword';
+export { timeDifference } from './timeDifference';
