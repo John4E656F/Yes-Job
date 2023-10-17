7696b9fa706beffaa8ded298fc804f34b49d237e
John4E656F <jm49andres@gmail.com>
2023-10-17 12:50:21 +0200

    build portfolio page

--- app/[locale]/profile/[id]/page.tsx
+++ app/[locale]/profile/[id]/page.tsx
@@ -1,22 +1,24 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import { supabase } from '@/supabase/supabase';
-import { Image, Label, Link } from '@/components';
+import { Image, Link, ListingCard } from '@/components';
 import type { UsersTypes, FormData } from '@/types';
 import { useStore } from '@/lib/store';
-import { HiArrowLongLeft, HiUser } from 'react-icons/hi2';
+import { HiUser } from 'react-icons/hi2';
+import { useTranslations } from 'next-intl';
 
 export default function ProfilePage({ params }: { params: { id: number } }) {
+  const t = useTranslations('app');
   const [userData, setUserData] = useState<UsersTypes>();
   const [userPosts, setUserPosts] = useState<FormData[]>([]);
+  const [isOwner, setIsOwner] = useState(false);
   const storedUserData = useStore((state) => state);
 
   useEffect(() => {
     const fetchUserDataAndPosts = async () => {
       try {
-        let fetchedUserData = storedUserData;
+        let fetchedUserData;
 
-        // If userData is not in store, fetch it
         if (!fetchedUserData) {
           const { data, error } = await supabase.from('users').select('*').eq('id', params.id).single();
 
@@ -27,9 +29,14 @@ export default function ProfilePage({ params }: { params: { id: number } }) {
 
           fetchedUserData = data;
         }
-        console.log(fetchedUserData);
 
-        setUserData(fetchedUserData);
+        setIsOwner(storedUserData?.id === fetchedUserData?.id);
+
+        if (storedUserData?.id !== fetchedUserData?.id) {
+          setUserData(fetchedUserData);
+        } else {
+          setUserData(storedUserData);
+        }
 
         if (fetchedUserData && fetchedUserData.isCompany) {
           const { data: postsData, error: postsError } = await supabase.from('jobPosting').select('*').eq('companyId', params.id);
@@ -47,26 +54,40 @@ export default function ProfilePage({ params }: { params: { id: number } }) {
     };
 
     fetchUserDataAndPosts();
-  }, [params.id, storedUserData]); // Dependency array - fetch data when id changes
+  }, [params.id, storedUserData]);
+  console.log(userPosts);
 
   return (
     <header className='w-full flex justify-center py-4 bg-brand-lightbg'>
-      <section className='container flex flex-col gap-4'>
-        <Link href='/' className='flex items-center gap-1 pl-5'>
-          <HiArrowLongLeft size={20} />
-          Back
-        </Link>
-        <div className='flex bg-white p-4 lg:p-8 gap-6'>
+      <section className='container bg-white flex flex-col py-4 gap-4'>
+        <div className='flex p-4 lg:p-8 gap-6'>
           {userData?.user_logo ? (
             <Image src={userData.user_logo} alt='user avatar' width={100} height={100} className='rounded-full p-1 ring-2 ring-gray-300' />
           ) : (
             <HiUser size={100} className='rounded-full p-1 ring-2 ring-gray-300 text-gray-400' aria-label='profile user avatar' />
           )}
-          <div>
-            <h2>{userData?.user_name}</h2>
-            <h3>{userData?.user_email}</h3>
+          <div className='flex flex-col justify-center'>
+            <h2 className='text-base font-semibold md:text-lg'>{userData?.user_name}</h2>
+            <h3 className='text-base md:text-lg'>{userData?.user_email}</h3>
           </div>
         </div>
+        <div className='w-full h-px bg-slate-300 rounded' />
+        {isOwner ? (
+          userPosts.map((jobPost) => {
+            const adaptedJobPost = {
+              ...jobPost, // spread all existing jobPost properties
+              companyId: {
+                user_name: userData!.user_name || '',
+                user_email: userData!.user_email || '',
+                user_logo: userData!.user_logo,
+                isCompany: userData!.isCompany,
+              },
+            };
+            return <ListingCard key={jobPost.id} jobPost={adaptedJobPost} t={t} />;
+          })
+        ) : (
+          <div></div>
+        )}
       </section>
     </header>
   );
