c1f1576b9db46d24b02d6d82b494094a81a06638
John4E656F <jm49andres@gmail.com>
2023-10-24 14:12:14 +0200

    Clean un used codes

--- utils/index.tsx
+++ utils/index.tsx
@@ -1,7 +1,5 @@
 export { formatDescription } from './formatDescription';
 export { formatDate } from './formatDate';
-export { generatePassword } from './generatePassword';
 export { timeDifference } from './timeDifference';
-export { getOrCreateCompanyId } from './getOrCreateCompanyId';
 export { registerNewCompany } from './registerNewCompany';
 export { removeSpaces } from './removeSpaces';
