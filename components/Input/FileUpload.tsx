fb7e13d6b7a27ea494df5627d1f9cc4b23cc75bd
John4E656F <jm49andres@gmail.com>
2023-10-26 14:51:14 +0200

    Implemented Validation to application form

--- components/Input/FileUpload.tsx
+++ components/Input/FileUpload.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState } from 'react';
 import { UseFormRegisterReturn } from 'react-hook-form';
 import { BiUpload } from 'react-icons/bi';
@@ -34,11 +35,10 @@ export function FileUpload({ register, error, label }: FileUploadProps) {
           className='flex flex-col items-center justify-center w-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-100'
         >
           <div className='flex flex-col items-center justify-center pt-5 pb-6'>
-            <BiUpload />
-            <p className='mb-2 text-sm text-gray-500'>
+            <BiUpload size={30} />
+            <p className='mb-2 text-sm text-gray-500 flex flex-col text-center gap-2'>
               <span className='font-semibold'>Upload</span> Drag 'n' drop some files here, or click to select files
             </p>
-            <p className='text-xs text-gray-500'>Upload Details</p>
           </div>
           <input id={`input${label}`} type='file' accept='.pdf,.doc' onChange={handleFileChange} ref={ref} className='hidden' {...rest} />
         </label>
