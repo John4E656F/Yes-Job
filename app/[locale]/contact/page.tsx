b6737a01ddcaa08f547dd0e3e067954ce7dff11b
John4E656F <jm49andres@gmail.com>
2023-10-27 13:50:06 +0200

    create zod schema, resolver and rhf for the contact page

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,60 +1,37 @@
 'use client';
 import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { Image, Toast } from '@/components';
+import { Image, Toast, FormInput, FormTextarea } from '@/components';
 import { useToggle } from '@/hooks';
 import { ToastTitle } from '@/types';
-
-interface ContactFormData {
-  fullName: string;
-  email: string;
-  message: string;
-}
-
-const validateEmail = (email: string) => {
-  // Simple email regex - you might want to use a library for this in real application
-  return /\S+@\S+\.\S+/.test(email);
-};
+import { useTranslations } from 'next-intl';
+import { useForm } from 'react-hook-form';
+import { contactFormResolver, type contactFormInputs } from './contactFormResolver';
 
 const ContactPage = () => {
-  const [formData, setFormData] = useState<ContactFormData>({
-    fullName: '',
-    email: '',
-    message: '',
-  });
-
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const t = useTranslations('app');
   const [isSubmitSuccessful, setIsSubmitSuccessful] = useState<boolean | null>(null);
   const { currentState: isToastOpen, toggleState: toggleToast } = useToggle(false);
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<contactFormInputs>({
+    resolver: contactFormResolver,
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    defaultValues: {
+      fullName: '',
+      email: '',
+      message: '',
+    },
+  });
   const handleCloseToast = () => {
     toggleToast(!isToastOpen);
   };
 
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target;
-    setFormData((prev) => ({ ...prev, [name]: value }));
-
-    if (name === 'email' && !validateEmail(value)) {
-      setErrors((prev) => ({ ...prev, [name]: 'Please enter a valid email address.' }));
-    } else {
-      setErrors((prev) => ({ ...prev, [name]: '' }));
-    }
-  };
-
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
-    // Validate the form data before submitting
-    if (!formData.fullName || !formData.email || !formData.message) {
-      setErrors((prev) => ({
-        ...prev,
-        fullName: !formData.fullName ? 'Full name is required' : '',
-        email: !formData.email ? 'Email is required' : '',
-        message: !formData.message ? 'Message is required' : '',
-      }));
-      return;
-    }
-
+  const onSubmit = async (data: contactFormInputs) => {
     // Make a POST request to the Formspark endpoint
     const response = await fetch('https://submit-form.com/PSmSObp9', {
       method: 'POST',
@@ -63,17 +40,13 @@ const ContactPage = () => {
         Accept: 'application/json',
       },
       body: JSON.stringify({
-        fullName: formData.fullName,
-        email: formData.email,
-        message: formData.message,
+        fullName: data.fullName,
+        email: data.email,
+        message: data.message,
       }),
     });
     if (response.ok) {
-      setFormData({
-        fullName: '',
-        email: '',
-        message: '',
-      });
+      reset();
       setIsSubmitSuccessful(true);
       toggleToast(!isToastOpen);
 
@@ -110,46 +83,20 @@ const ContactPage = () => {
         </div>
       </div>
       <section className='w-full flex justify-center bg-brand-lightbg py-4 md:py-8'>
-        <form className='flex flex-col container py-2 gap-4' onSubmit={handleSubmit}>
+        <form className='flex flex-col container py-2 gap-4' onSubmit={handleSubmit(onSubmit)}>
           <h2 className='text-4xl font-semibold'>Contact us</h2>
           <div className='flex flex-col bg-white p-4 lg:p-5 gap-6'>
-            <div className='flex flex-col gap-3'>
-              <label className='text-lg font-medium'>Full name *</label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                name='fullName'
-                value={formData.fullName}
-                onChange={handleInputChange}
-                type='text'
-                placeholder='John'
-                required
-              />
-              {errors.fullName && <p className='text-red-500'>{errors.fullName}</p>}
-            </div>
-            <div className='flex flex-col gap-3'>
-              <label className='text-lg font-medium'>Email address *</label>
-              <input
-                className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                name='email'
-                value={formData.email}
-                onChange={handleInputChange}
-                type='text'
-                placeholder='Your email address'
-                required
-              />
-              {errors.email && <p className='text-red-500'>{errors.email}</p>}
-            </div>
-            <div className='flex flex-col gap-3'>
-              <label className='text-lg font-medium'>Message *</label>
-              <textarea
-                className='shadow appearance-none border rounded w-full h-60 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-                name='message'
-                value={formData.message}
-                onChange={handleInputChange}
-                placeholder='Hello'
-              />
-              {errors.message && <p className='text-red-500'>{errors.message}</p>}
-            </div>
+            <FormInput placeholder='Raymond Albert Kroc' label='Full Name' type='text' register={register('fullName')} error={errors.fullName} />
+            <FormInput
+              label='Email address'
+              type='text'
+              register={register('email')}
+              error={errors.email}
+              invalidEmail={t('error.invalidEmail')}
+              placeholder='kroc@gmail.com'
+            />
+
+            <FormTextarea label='Message' register={register('message')} error={errors.message} isRequiredMessage='' placeholder='' />
           </div>
           <button
             type='submit'
