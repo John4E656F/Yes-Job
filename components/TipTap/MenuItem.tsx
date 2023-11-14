1733e3df2922de560a61b695561c34a58570102a
John4E656F <jm49andres@gmail.com>
2023-11-14 20:11:25 +0100

    update ruchtext menu bar

--- components/TipTap/MenuItem.tsx
+++ components/TipTap/MenuItem.tsx
@@ -9,7 +9,9 @@ export interface MenuItemProps {
 
 export const MenuItem: FC<MenuItemProps> = ({ icon, title, action, isActive = null }) => (
   <button
-    className={`bg-transparent border-none rounded-md text-black cursor-pointer w-10 h-10 mr-1 p-1${isActive && isActive() ? ' is-active' : ''}`}
+    className={`bg-transparent border-none rounded-md text-black cursor-pointer w-10 h-10 mr-1 p-1 px-2 hover:bg-gray-200 active:bg-gray-200 ${
+      isActive && isActive() ? 'bg-gray-200' : ''
+    }`}
     onClick={action}
     title={title}
     type='button'
