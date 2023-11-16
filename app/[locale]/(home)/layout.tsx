b15225f39c7d1b735aef0c1f69fc48feb97ead40
John4E656F <jm49andres@gmail.com>
2023-11-16 18:30:56 +0100

    Implementing nested layout

---
+++ app/[locale]/(home)/layout.tsx
@@ -0,0 +1,43 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { notFound } from 'next/navigation';
+import { Navbar, Footer } from '@/components';
+
+interface HomeLayoutProps {
+  children: React.ReactNode;
+  params: { locale: string };
+}
+
+export default async function HomeLayout({ children, params }: HomeLayoutProps) {
+  const validLocales = ['en', 'fr', 'nl'];
+  let locale = params?.locale ?? 'en';
+
+  if (!validLocales.includes(locale)) {
+    // logger.warn(`Invalid locale "${locale}" provided. Defaulting to "en".`);
+    locale = 'en';
+  }
+
+  let translation;
+  try {
+    translation = (await import(`../../../translation/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return (
+    <>
+      <header>
+        <Navbar currentLocale={locale} session={session} />
+      </header>
+      <section>
+        <main className='flex flex-col items-center'>{children}</main>
+      </section>
+      <Footer />
+    </>
+  );
+}
