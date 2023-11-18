01d897d0021b0f336b1b3e87441df2fd4a732985
John4E656F <jm49andres@gmail.com>
2023-11-18 14:32:32 +0100

    Implemented server action and building job listing page

--- components/Link/Link.tsx
+++ components/Link/Link.tsx
@@ -7,9 +7,9 @@ type LinkProps = {
   onClick?: (event: React.MouseEvent) => void;
 } & NextLinkProps;
 
-export function Link({ href, children, className, onClick }: LinkProps) {
+export function Link({ href, as, children, className, onClick }: LinkProps) {
   return (
-    <NextLink href={href} className={className} onClick={onClick}>
+    <NextLink href={href} as={as} className={className} onClick={onClick}>
       {children}
     </NextLink>
   );
