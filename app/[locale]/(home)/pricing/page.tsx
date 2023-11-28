409743f5de4e4606047c37b6bae80bcc5af53efe
John4E656F <jm49andres@gmail.com>
2023-11-28 13:26:28 +0100

    Created Company type

---
+++ app/[locale]/(home)/pricing/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const pricingPage = () => {
+  return (
+    <section>
+      <div>
+        <h1>Pricing</h1>
+      </div>
+    </section>
+  );
+};
+
+export default pricingPage;
