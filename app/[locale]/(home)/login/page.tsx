65639351f916adba0b15e7424eddbc7af092c202
John4E656F <jm49andres@gmail.com>
2023-11-30 12:13:49 +0100

    Building auth

--- app/[locale]/(home)/login/page.tsx
+++ app/[locale]/(home)/login/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import { Link } from '@/components';
 import { headers, cookies } from 'next/headers';
 import { createClient } from '@/utils/supabase/server';
 import { redirect } from 'next/navigation';
@@ -50,7 +50,11 @@ export default async function Login({ searchParams }: { searchParams: { message:
   };
 
   return (
-    <div className='flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2'>
+    <div className='flex-1 flex flex-col w-full px-8 py-16 sm:max-w-md justify-center gap-2'>
+      <div className='flex flex-col gap-2 mb-6'>
+        <h1 className='text-3xl font-bold text-center'>Log in to your account</h1>
+        <p className='text-center'>Welcome back! Please enter your details.</p>
+      </div>
       <form className='animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground' action={signIn}>
         <label className='text-md' htmlFor='email'>
           {t('login.email')}
@@ -61,9 +65,14 @@ export default async function Login({ searchParams }: { searchParams: { message:
         </label>
         <input className='rounded-md px-4 py-2 bg-inherit border mb-6' type='password' name='password' placeholder='••••••••' required />
         <button className='bg-brand-primary text-white rounded-md px-4 py-2 text-foreground mb-2'>{t('login.signIn')}</button>
-        <button formAction={signUp} className='border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2'>
-          {t('login.signUp')}
-        </button>
+        <div className='flex justify-center gap-2'>
+          <label className='text-md' htmlFor='signup'>
+            Don't have an account?
+          </label>
+          <Link href='/signup' className='text-brand-secondary'>
+            {t('login.signUp')}
+          </Link>
+        </div>
         {searchParams?.message && <p className='mt-4 p-4 bg-foreground/10 text-foreground text-center'>{searchParams.message}</p>}
       </form>
     </div>
