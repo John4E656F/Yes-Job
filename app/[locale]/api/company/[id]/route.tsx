82ab74ca882e817ada44488ce628b2d99163243a
John4E656F <jm49andres@gmail.com>
2023-12-02 13:30:16 +0100

    Update First Publish if company details doesnt exist

--- app/[locale]/api/company/[id]/route.tsx
+++ app/[locale]/api/company/[id]/route.tsx
@@ -8,11 +8,7 @@ export async function GET(request: Request, { params }: { params: { id: string }
   const ownerId = params.id;
 
   try {
-    const { data: fetchedCompanyData, error: fetchedCompanyError } = await supabase
-      .from('company')
-      .select('*')
-      .eq('owner_id' || 'teamMembers', ownerId)
-      .single();
+    const { data: fetchedCompanyData, error: fetchedCompanyError } = await supabase.from('company').select('*').eq('owner_id', ownerId).single();
 
     if (fetchedCompanyError) {
       return NextResponse.json({ fetchedCompanyError: 'Failed to fetch user data: ' + fetchedCompanyError.message });
