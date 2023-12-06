6bda78adcff0a1da5ee3ec485f0d410bdd46a5ac
John4E656F <jm49andres@gmail.com>
2023-12-06 19:10:52 +0100

    Building Company Setting page

--- app/[locale]/dashboard/company/page.tsx
+++ app/[locale]/dashboard/company/page.tsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Link, Divider, DashboardListingCard } from '@/components';
 
-export default function CompanyPage() {
+import { getServerUserSession } from '@/lib/actions/getServerUserSession';
+import { refreshUserSession } from '@/lib/actions/refreshServerSession';
+import { getTranslations } from 'next-intl/server';
+export default async function CompanyPage() {
+  const t = await getTranslations('dashboard');
+  const session = await getServerUserSession();
+  let sessionId;
+  if (session) {
+    sessionId = session.user.id;
+  } else {
+    const session = await refreshUserSession();
+    sessionId = session.user.id;
+  }
+
+  const userResponse = await fetch(
+    process.env.NEXT_PRIVATE_PRODUCTION === 'true'
+      ? process.env.NEXT_PRIVATE_PRODUCTION_URL + `/api/user/${sessionId}`
+      : process.env.NEXT_PRIVATE_URL + `/api/user/${sessionId}`,
+  );
+  const { fetchedUserData } = await userResponse.json();
+  console.log('fetchedUserData', fetchedUserData);
+
+  const companyResponse = await fetch(
+    process.env.NEXT_PRIVATE_PRODUCTION === 'true'
+      ? process.env.NEXT_PRIVATE_PRODUCTION_URL + `/api/company/${fetchedUserData.id}`
+      : process.env.NEXT_PRIVATE_URL + `/api/company/${fetchedUserData.id}`,
+  );
+  const { fetchedCompanyData } = await companyResponse.json();
+  console.log('fetchedCompanyData', fetchedCompanyData);
+
   return (
-    <section>
-      <div>Hello World</div>
+    <section className='w-full bg-white flex flex-col py-4 pt-10 px-5 gap-y-8'>
+      <div className='flex flex-col gap-2'>
+        <div className='flex justify-between'>
+          <h1>Company</h1>
+          <Link
+            href='/annonce/publier'
+            className='flex items-center justify-center text-center bg-brand-primary text-white rounded-lg hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-gray-200'
+          >
+            <button type='button' className='px-4 h-11 text-sm'>
+              View Company Profile
+            </button>
+          </Link>
+        </div>
+      </div>
       <div>Hello</div>
     </section>
   );
