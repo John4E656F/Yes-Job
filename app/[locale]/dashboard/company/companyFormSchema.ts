bd79312fe7f26e8c5eb8964dfcc48866940ca9e6
John4E656F <jm49andres@gmail.com>
2023-12-07 17:34:50 +0100

    Workinbg ofr dashboard company page form

--- app/[locale]/dashboard/company/companyFormSchema.ts
+++ app/[locale]/dashboard/company/companyFormSchema.ts
@@ -11,6 +11,7 @@ export const companyFormSchema = z.object({
     .transform((val) => val.toLowerCase().replace(/\s/g, '')),
   website: z.string().url().optional().or(z.literal('')),
   logo: z.union([z.string(), z.any()]).nullable(),
+  about: z.string().max(275).optional(),
   address: z
     .string()
     .min(1)
--- components/Dashboard/index.tsx
+++ components/Dashboard/index.tsx
@@ -2,3 +2,5 @@ export { Sidebar } from './Sidebar/Sidebar';
 export { SidebarList } from './Sidebar/SidebarList';
 export { MobileNavbar } from './MobileNavbar/MobileNavbar';
 export { DashboardFormInput } from './Input/FormInput';
+export { DashboardImageUpload } from './Input/ImageUpload';
+export { DashboardFormTextarea } from './Input/FormTextarea';
