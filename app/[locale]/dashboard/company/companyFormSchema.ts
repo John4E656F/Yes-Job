1eda6aaae4686c800ed1f6d3681ce8b52dce85a4
John4E656F <jm49andres@gmail.com>
2023-12-07 13:04:37 +0100

    Working on company Form

---
+++ app/[locale]/dashboard/company/companyFormSchema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const companyFormSchema = z.object({
+  name: z
+    .string()
+    .min(1)
+    .transform((val) => val.toLowerCase()),
+  slug: z
+    .string()
+    .min(1)
+    .transform((val) => val.toLowerCase().replace(/\s/g, '')),
+  website: z.string().url().optional().or(z.literal('')),
+  logo: z.union([z.string(), z.any()]).nullable(),
+  address: z
+    .string()
+    .min(1)
+    .transform((val) => val.toLowerCase()),
+});
