c68882776dd917e28c1a2206106b2dd3aac89958
John4E656F <jm49andres@gmail.com>
2023-12-09 20:13:52 +0100

    Finish Setting profile info page

--- lib/actions/userProfile/index.tsx
+++ lib/actions/userProfile/index.tsx
@@ -12,23 +12,28 @@ interface UserProps {
 
 export async function updateUser({ userData, profilePictureUrl, path }: UserProps) {
   const supabase = createClient();
-  const { data: insertedUserData, error: insertedUserDataError } = await supabase
-    .from('users')
-    .update({
-      firstname: userData.firstname,
-      lastname: userData.lastname,
-      user_email: userData.user_email,
-      user_phone: userData.user_phone,
-      profile_picture: profilePictureUrl,
-    })
-    .eq('id', userData.user_id);
-
-  if (insertedUserDataError) {
-    console.log('error', insertedUserDataError);
-
-    return { type: 'error' as const, message: insertedUserDataError.message };
+  const { data, error } = await supabase.auth.updateUser({ email: userData.user_email });
+  if (error) {
+    return { type: 'error' as const, message: error.message };
   } else {
-    revalidatePath(path!);
-    return { type: 'success' as const, message: 'Company has been updated successfully' };
+    const { data: insertedUserData, error: insertedUserDataError } = await supabase
+      .from('users')
+      .update({
+        firstname: userData.firstname,
+        lastname: userData.lastname,
+        user_email: userData.user_email,
+        user_phone: userData.user_phone,
+        profile_picture: profilePictureUrl,
+      })
+      .eq('id', userData.user_id);
+
+    if (insertedUserDataError) {
+      return { type: 'error' as const, message: insertedUserDataError.message };
+    } else {
+      //   console.log('data', data);
+
+      revalidatePath(path!);
+      return { type: 'success' as const, message: 'Company has been updated successfully' };
+    }
   }
 }
