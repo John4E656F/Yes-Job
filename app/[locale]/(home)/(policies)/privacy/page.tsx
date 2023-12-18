ac27d5981548abb32751e616447f8ceb3d553928
John4E656F <jm49andres@gmail.com>
2023-12-18 16:56:27 +0100

    Build privacy policies

--- app/[locale]/(home)/(policies)/privacy/page.tsx
+++ app/[locale]/(home)/(policies)/privacy/page.tsx
@@ -50,18 +50,142 @@ export default function PrivacyPolicyPage() {
           <div className='flex flex-col gap-2'>
             <h3>2. Types of personal information we collect</h3>
             <p>
-              The type of personal information we may collect includes, but is not limited to, your name, email address, professional qualifications,
-              work experience, photographs, country of residence, and job preferences. Account creation requires your name, email address, and a
-              password. You may choose the information to include in your profile, such as work preferences, education, and skills, to enhance the
-              effectiveness of our Services. We advise against posting sensitive personal data that you would not want publicly available.
+              The personal information we collect varies based on whether you are using Yes Job as a company or as a candidate. This information
+              includes, but is not limited to:
+            </p>
+            <p>For Companies:</p>
+            <ul>
+              <li>
+                <b>Company Information: </b>Your company name, contact details, and relevant information about your business.
+              </li>
+              <li>
+                <b>Job Offer Details: </b>Information related to job offers posted, including job descriptions, requirements, and contact information
+                for applicants.
+              </li>
+              <li>
+                <b>Account Information: </b>Account creation for companies requires a company name, email address, and a password.
+              </li>
+              <li>
+                <b>Professional Qualifications: </b>Information about the qualifications or skills you are seeking in candidates.
+              </li>
+            </ul>
+            <p>For Candidates (Upcoming Feature):</p>
+            <ul>
+              <li>
+                <b>Personal Information: </b>Your name, email address, professional qualifications, work experience, photographs, and country of
+                residence.
+              </li>
+              <li>
+                <b>Job Preferences: </b>Information about the types of jobs you are interested in, preferred work locations, and other job-related
+                preferences.
+              </li>
+              <li>
+                <b>CV and Professional Details: </b>Your curriculum vitae, education, skills, and any other information relevant to job applications.
+              </li>
+              <li>
+                <b>Account Information: </b>Account creation for candidates will require your name, email address, and a password.
+              </li>
+            </ul>
+            <p>
+              For both types of users, you may choose the information to include in your profile to enhance the effectiveness of our Services. We
+              advise against posting sensitive personal data that you would not want publicly available.
             </p>
             <p>
-              Personal data is also collected when you interact with our Services, including filling out forms, responding to surveys, or submitting
-              job applications. We log usage data, employ cookies when you use our Services. We do not collect device and network information
+              Additionally, we collect personal data when you interact with our Services, such as filling out forms, responding to surveys, or
+              submitting job applications. We also log usage data, employ cookies, and collect device and network information when you use our
+              Services.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>3. Our purposes for handling your personal information</h3>
+            <p>We collect, hold, use, and disclose personal information to:</p>
+            <ul>
+              <li>Offer and provide our Services.</li>
+              <li>Manage and administer those Services, including account management.</li>
+              <li>Communicate with you about our Services.</li>
+              <li>Verify and manage your specified membership with a company.</li>
+              <li>Display your public personal profile as provided in your profile forms.</li>
+              <li>Comply with our legal and regulatory obligations.</li>
+              <li>Manage our business effectively.</li>
+            </ul>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>4. Disclosure of Personal Information</h3>
+            <p>
+              We may disclose personal information between our organizations or to third parties such as our suppliers, technical and support service
+              providers, or professional advisors, where permitted by GDPR. If we disclose information to a third party, we ensure that the third
+              party protects your information to the same extent that we do. Specifically, we may disclose personal information for purposes outlined
+              in this Privacy Policy to:
+            </p>
+            <ul>
+              <li>Cloud service providers.</li>
+              <li>Email providers.</li>
+              <li>Analytics services.</li>
+              <li>Payment system operators.</li>
+            </ul>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>5. Protection of Personal Information</h3>
+            <p>
+              We hold personal information in secure physical records, electronically on our intranet system, in cloud storage, and in some cases, on
+              third-party servers, potentially located overseas. We maintain appropriate physical, procedural, and technical security to prevent loss,
+              misuse, unauthorized access, disclosure, or modification of personal information. Access to personal information is restricted to those
+              who need it to perform their job. We will destroy or de-identify personal information once it is no longer needed.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>6. Direct Marketing</h3>
+            <p>
+              We believe in the value of our Services and, as such, we may send you information about new products, services, and promotions. We will
+              not disclose your personal information to third parties for marketing purposes without your consent. You may opt out at any time if you
+              do not wish to receive commercial messages from us.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>7. Cookies</h3>
+            <p>
+              Cookies are small text files stored on your computer for a predefined period. We use cookies to identify specific machines and collect
+              aggregate information on site usage. This helps us adapt the Site to better suit user needs. For information on cookie settings, please
+              refer to your browser's manual.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>8. Links to Other Websites</h3>
+            <p>
+              Our website, Yes Job, may contain links to other websites for your convenience. This includes links to recruiters' websites and other
+              third-party sites relevant to job seeking and employment. Please note that we are not responsible for the privacy practices or the
+              content of these other websites. When you access third-party websites, such as recruiters' sites, through our platform, their own terms
+              and privacy policies will apply. We encourage our users to be aware when they leave our site and to read the privacy statements of each
+              and every website that collects personally identifiable information.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>9. Accessing and Correcting Your Personal Information</h3>
+            <p>
+              You may contact our Privacy Officer to access or correct your personal information. We will respond to requests within a reasonable time
+              frame.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>10. Overseas Transfers of Personal Information</h3>
+            <p>
+              From time to time, we may engage overseas recipients for services such as cloud-based storage solutions. By providing us with your
+              personal information, you consent to the storage of such information on overseas servers.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>11. Resolving Personal Information Concerns</h3>
+            <p>
+              If you have any questions or complaints about this Privacy Policy or how we handle your personal information, please contact our Privacy
+              Officer. We take all complaints seriously and will respond within a reasonable period.
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>12. Changes to This Privacy Policy</h3>
+            <p>
+              We reserve the right to change this Privacy Policy from time to time. The most current version of our Privacy Policy is available on our
+              website.
+            </p>
           </div>
         </div>
       </section>
