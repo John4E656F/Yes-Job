bc1e2e5e1123399846edfc12df90df42ce02f283
John4E656F <jm49andres@gmail.com>
2023-12-20 19:16:10 +0100

    Fix TOS

--- app/[locale]/(home)/(policies)/terms/page.tsx
+++ app/[locale]/(home)/(policies)/terms/page.tsx
@@ -22,14 +22,14 @@ export default function TermsAndConditionsGeneralServicesPage() {
           <div className='flex flex-col gap-2'>
             <h3 className='text-left'>Terms</h3>
             <p>
-              <b>1. Usage Restrictions: </b> If you do not agree with any of these terms, you are prohibited from using or accessing this site. The
+              1. <b> Usage Restrictions: </b> If you do not agree with any of these terms, you are prohibited from using or accessing this site. The
               materials contained in this website are protected by applicable copyright and trademark law.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Use License</h3>
             <p>
-              <b>2.Scope of License: </b> Permission is granted to temporarily download one copy of the materials (information or software) on Yes
+              2. <b>Scope of License: </b> Permission is granted to temporarily download one copy of the materials (information or software) on Yes
               Job's website for personal, non-commercial transitory viewing only. Under this license you may not:
             </p>
             <ul>
@@ -40,7 +40,7 @@ export default function TermsAndConditionsGeneralServicesPage() {
               <li>Transfer the materials to another person or 'mirror' the materials on any other server.</li>
             </ul>
             <p>
-              <b>3. License Termination: </b> This license shall automatically terminate if you violate any of these restrictions and may be
+              3. <b> License Termination: </b> This license shall automatically terminate if you violate any of these restrictions and may be
               terminated by Yes Job at any time. Upon terminating your viewing of these materials, you must destroy any downloaded materials in your
               possession whether in electronic or printed format.
             </p>
@@ -48,7 +48,7 @@ export default function TermsAndConditionsGeneralServicesPage() {
           <div className='flex flex-col gap-2'>
             <h3>Disclaimer</h3>
             <p>
-              <b>4. As Is Basis: </b> The materials on Yes Job's website are provided on an 'as is' basis. Yes Job makes no warranties, expressed or
+              4. <b> As Is Basis: </b> The materials on Yes Job's website are provided on an 'as is' basis. Yes Job makes no warranties, expressed or
               implied, and disclaims all other warranties including, without limitation, implied warranties of merchantability, fitness for a
               particular purpose, or non-infringement of intellectual property or other violation of rights.
             </p>
@@ -56,7 +56,7 @@ export default function TermsAndConditionsGeneralServicesPage() {
           <div className='flex flex-col gap-2'>
             <h3>Limitations</h3>
             <p>
-              <b>5. Liability Limitations: </b> In no event shall Yes Job or its suppliers be liable for any damages arising out of the use or
+              5. <b> Liability Limitations: </b> In no event shall Yes Job or its suppliers be liable for any damages arising out of the use or
               inability to use the materials on Yes Job's website, even if Yes Job or an authorized representative has been notified orally or in
               writing of the possibility of such damage.
             </p>
@@ -64,84 +64,84 @@ export default function TermsAndConditionsGeneralServicesPage() {
           <div className='flex flex-col gap-2'>
             <h3>Accuracy of Materials</h3>
             <p>
-              <b>6. Content Accuracy: </b> The materials appearing on Yes Job's website could include technical, typographical, or photographic
+              6. <b> Content Accuracy: </b> The materials appearing on Yes Job's website could include technical, typographical, or photographic
               errors. Yes Job does not warrant that any of the materials on its website are accurate, complete, or current.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Links</h3>
             <p>
-              <b>7. External Links: </b> Yes Job has not reviewed all sites linked to its website and is not responsible for the contents of any such
+              7. <b> External Links: </b> Yes Job has not reviewed all sites linked to its website and is not responsible for the contents of any such
               linked site. The inclusion of any link does not imply endorsement by Yes Job. Use of any such linked website is at the user's own risk.{' '}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Modifications</h3>
             <p>
-              <b>8. Terms Revision: </b> Yes Job may revise these terms of service for its website at any time without notice. By using this website,
+              8. <b> Terms Revision: </b> Yes Job may revise these terms of service for its website at any time without notice. By using this website,
               you agree to be bound by the current version of these terms of service.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Governing Law</h3>
             <p>
-              <b>9. Jurisdiction: </b> These Terms are governed by the laws of Belgium, without regard to its conflict of law provisions. By using Yes
-              Job, you agree to submit to the exclusive jurisdiction of the courts located in Belgium for the resolution of any disputes arising from
-              or related to these Terms or your use of the Service.
+              9. <b> Jurisdiction: </b> These Terms are governed by the laws of Belgium, without regard to its conflict of law provisions. By using
+              Yes Job, you agree to submit to the exclusive jurisdiction of the courts located in Belgium for the resolution of any disputes arising
+              from or related to these Terms or your use of the Service.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Services</h3>
             <p>
-              <b>10. Scope of Services: </b> These terms and conditions apply to the Yes Job website, communications, and services. Registered users
+              10. <b> Scope of Services: </b> These terms and conditions apply to the Yes Job website, communications, and services. Registered users
               are “Members,” and unregistered users are “Visitors.” Members who have signed up for employer accounts are “Employers,” while job
               seekers are “Job Seekers.” Visitors or Members who apply for a job using the Site are “Candidates.”{' '}
             </p>
             <p>
-              <b>11. Site Ownership: </b> The Site is owned and operated by Yes Job (defined as “Yes Job”, “we”, “us” and “our”). Your continued use
+              11. <b> Site Ownership: </b> The Site is owned and operated by Yes Job (defined as “Yes Job”, “we”, “us” and “our”). Your continued use
               of this Site signifies your acceptance of these Terms.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Variation of Terms</h3>
             <p>
-              <b>12. Terms Update: </b> We may update these Terms at any time, with notice published on the Site.
+              12. <b> Terms Update: </b> We may update these Terms at any time, with notice published on the Site.
             </p>
             <p>
-              <b>13. Notification of Changes: </b> Changes to the Terms will be communicated with reasonable notice, especially for changes materially
+              13.<b> Notification of Changes: </b> Changes to the Terms will be communicated with reasonable notice, especially for changes materially
               detrimental to you.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Intellectual Property Rights</h3>
             <p>
-              <b>14. Ownership:</b> Yes Job retains all intellectual property rights in the services provided to you.
+              14.<b> Ownership:</b> Yes Job retains all intellectual property rights in the services provided to you.
             </p>
           </div>
           <div className='flex flex-col gap-2'>
             <h3>Limitation of Liability and Disclaimer</h3>
             <p>
-              <b>15. Service Warranty: </b> Yes Job provides no warranty that services available through the Site will be uninterrupted or error-free.
-              Our liability is limited to the maximum extent permitted by law.
+              15. <b> Service Warranty: </b> Yes Job provides no warranty that services available through the Site will be uninterrupted or
+              error-free. Our liability is limited to the maximum extent permitted by law.
             </p>
             <p>
-              <b>16. Liability Cap: </b> Liability for breach of any implied warranty, which cannot be excluded, is limited to the supply of the
+              16. <b> Liability Cap: </b> Liability for breach of any implied warranty, which cannot be excluded, is limited to the supply of the
               services again or the cost of having the services supplied again.
             </p>
             <p>
-              <b>17. Disclaimer Extent: </b> Our liability disclaimer applies to direct, indirect, and consequential damages.
+              17. <b> Disclaimer Extent: </b> Our liability disclaimer applies to direct, indirect, and consequential damages.
             </p>
             <p>
-              <b>18. Content Responsibility: </b> Yes Job accepts no responsibility for errors in your advertisements on our Site.
+              18. <b> Content Responsibility: </b> Yes Job accepts no responsibility for errors in your advertisements on our Site.
             </p>
             <p>
-              <b>19. Service Effort: </b> Yes Job will make reasonable efforts to publish advertisements promptly.
+              19. <b> Service Effort: </b> Yes Job will make reasonable efforts to publish advertisements promptly.
             </p>
             <p>
-              <b>20. Data Security: </b> Yes Job does not guarantee files available for download through the Site will be free of harmful elements.
+              20. <b> Data Security: </b> Yes Job does not guarantee files available for download through the Site will be free of harmful elements.
             </p>
             <p>
-              <b>21. Role as a Medium: </b> Yes Job acts as a medium for employment opportunities and does not vet Candidates or their
+              21. <b> Role as a Medium: </b> Yes Job acts as a medium for employment opportunities and does not vet Candidates or their
               representations.
             </p>
           </div>
