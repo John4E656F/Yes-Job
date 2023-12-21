311849a29ae8ff30885188941b226ccd8d47f174
John4E656F <jm49andres@gmail.com>
2023-12-21 19:23:19 +0100

    Create privacy i18n en

--- app/[locale]/(home)/(policies)/privacy/page.tsx
+++ app/[locale]/(home)/(policies)/privacy/page.tsx
@@ -1,104 +1,80 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
 
 export default function PrivacyPolicyPage() {
+  const t = useTranslations('privacy');
+
   return (
     <>
       <section className='w-full flex flex-col items-center px-10 justify-center py-10 bg-brand-lightbg'>
-        <div className=' w-full flex flex-col justify-center text-center items-center '>
-          <p className='text-sm'>Current as of 17/12/2023</p>
-          <h1>Privacy Policy</h1>
-          <p>We respect your privacy regarding any information we may collect from you across our website and strive to be 100% transparent.</p>
+        <div className='w-full flex flex-col justify-center text-center items-center'>
+          <p className='text-sm'>{t('currentDate')}</p>
+          <h1>{t('title')}</h1>
+          <p>{t('introText')}</p>
         </div>
       </section>
       <section className='w-full flex flex-col items-center justify-center py-4 bg-white'>
-        <div className='w-1/2 flex flex-col justify-center gap-5 mt-10 '>
+        <div className='w-1/2 flex flex-col justify-center gap-5 mt-10'>
           <div className='flex flex-col gap-2'>
-            <h2 className='text-left'>Privacy</h2>
-            <p>
-              Welcome to Yes Job (“the Site”). Your privacy is of utmost importance to us, and we are dedicated to protecting the information you
-              entrust to us. Yes Job (referred to as "we", "us", or "our") is a premier horeca job board in Belgium, connecting companies with
-              potential candidates. This privacy policy applies to our website, communications, and all services offered through Yes Job (“the
-              Services”).
-            </p>
-
-            <p>
-              Yes Job is committed to complying with the European Union’s General Data Protection Regulation (GDPR) and other relevant European data
-              protection laws. This Privacy Policy explains our approach to managing your personal information, including our obligations and your
-              rights in respect of our dealings with your personal information. We encourage you to read this Privacy Policy to understand how your
-              personal information is handled when collected via our website or as a result of using our Services.
-            </p>
+            <h2 className='text-left'>{t('section1.title')}</h2>
+            <p>{t('section1.paragraph1')}</p>
+            <p>{t('section1.paragraph2')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>1. How we collect your personal information</h3>
-            <p>
-              We collect and hold your personal information in a manner that is fair, lawful, and non-intrusive. Wherever practical, we collect your
-              personal information directly from you through means such as:
-            </p>
+            <h3>{t('section2.title')}</h3>
+            <p>{t('section2.paragraph1')}</p>
             <ul>
-              <li>Making an inquiry, subscribing, or signing up for our service through our website.</li>
-              <li>Contacting us via telephone or email.</li>
-              <li>Completing our online forms, using the chat feature on our website, or corresponding with us.</li>
-              <li>Participating in customer satisfaction and market research surveys.</li>
-              <li>Administering our Services.</li>
-              <li>Managing our business operations.</li>
+              <li>{t('section2.list1.item1')}</li>
+              <li>{t('section2.list1.item2')}</li>
+              <li>{t('section2.list1.item3')}</li>
+              <li>{t('section2.list1.item4')}</li>
+              <li>{t('section2.list1.item5')}</li>
+              <li>{t('section2.list1.item6')}</li>
             </ul>
-            <p>
-              Additionally, we may collect personal information from publicly available sources and third parties, including suppliers, recruitment
-              agencies, contractors, clients, and business partners.
-            </p>
+            <p>{t('section2.paragraph2')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>2. Types of personal information we collect</h3>
-            <p>
-              The personal information we collect varies based on whether you are using Yes Job as a company or as a candidate. This information
-              includes, but is not limited to:
-            </p>
-            <p>For Companies:</p>
+            <h3>{t('section3.title')}</h3>
+            <p>{t('section3.paragraph1')}</p>
+            <p>{t('section3.forCompanies')}</p>
             <ul>
               <li>
-                <b>Company Information: </b>Your company name, contact details, and relevant information about your business.
+                <b>Company Information:</b> Your company name, contact details, and relevant information about your business.
               </li>
               <li>
-                <b>Job Offer Details: </b>Information related to job offers posted, including job descriptions, requirements, and contact information
+                <b>Job Offer Details:</b> Information related to job offers posted, including job descriptions, requirements, and contact information
                 for applicants.
               </li>
               <li>
-                <b>Account Information: </b>Account creation for companies requires a company name, email address, and a password.
+                <b>Account Information:</b> Account creation for companies requires a company name, email address, and a password.
               </li>
               <li>
-                <b>Professional Qualifications: </b>Information about the qualifications or skills you are seeking in candidates.
+                <b>Professional Qualifications:</b> Information about the qualifications or skills you are seeking in candidates.
               </li>
             </ul>
-            <p>For Candidates (Upcoming Feature):</p>
+            <p>{t('section3.forCandidates')}</p>
             <ul>
               <li>
-                <b>Personal Information: </b>Your name, email address, professional qualifications, work experience, photographs, and country of
+                <b>Personal Information:</b> Your name, email address, professional qualifications, work experience, photographs, and country of
                 residence.
               </li>
               <li>
-                <b>Job Preferences: </b>Information about the types of jobs you are interested in, preferred work locations, and other job-related
+                <b>Job Preferences:</b> Information about the types of jobs you are interested in, preferred work locations, and other job-related
                 preferences.
               </li>
               <li>
-                <b>CV and Professional Details: </b>Your curriculum vitae, education, skills, and any other information relevant to job applications.
+                <b>CV and Professional Details:</b> Your curriculum vitae, education, skills, and any other information relevant to job applications.
               </li>
               <li>
-                <b>Account Information: </b>Account creation for candidates will require your name, email address, and a password.
+                <b>Account Information:</b> Account creation for candidates will require your name, email address, and a password.
               </li>
             </ul>
-            <p>
-              For both types of users, you may choose the information to include in your profile to enhance the effectiveness of our Services. We
-              advise against posting sensitive personal data that you would not want publicly available.
-            </p>
-            <p>
-              Additionally, we collect personal data when you interact with our Services, such as filling out forms, responding to surveys, or
-              submitting job applications. We also log usage data, employ cookies, and collect device and network information when you use our
-              Services.
-            </p>
+            <p>{t('section3.profileAdvice')}</p>
+            <p>{t('section3.interactionData')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>3. Our purposes for handling your personal information</h3>
-            <p>We collect, hold, use, and disclose personal information to:</p>
+            <h3>{t('section4.title')}</h3>
+            <p>{t('section4.paragraph1')}</p>
             <ul>
               <li>Offer and provide our Services.</li>
               <li>Manage and administer those Services, including account management.</li>
@@ -110,13 +86,8 @@ export default function PrivacyPolicyPage() {
             </ul>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>4. Disclosure of Personal Information</h3>
-            <p>
-              We may disclose personal information between our organizations or to third parties such as our suppliers, technical and support service
-              providers, or professional advisors, where permitted by GDPR. If we disclose information to a third party, we ensure that the third
-              party protects your information to the same extent that we do. Specifically, we may disclose personal information for purposes outlined
-              in this Privacy Policy to:
-            </p>
+            <h3>{t('section5.title')}</h3>
+            <p>{t('section5.paragraph1')}</p>
             <ul>
               <li>Cloud service providers.</li>
               <li>Email providers.</li>
@@ -125,67 +96,32 @@ export default function PrivacyPolicyPage() {
             </ul>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>5. Protection of Personal Information</h3>
-            <p>
-              We hold personal information in secure physical records, electronically on our intranet system, in cloud storage, and in some cases, on
-              third-party servers, potentially located overseas. We maintain appropriate physical, procedural, and technical security to prevent loss,
-              misuse, unauthorized access, disclosure, or modification of personal information. Access to personal information is restricted to those
-              who need it to perform their job. We will destroy or de-identify personal information once it is no longer needed.
-            </p>
+            <h3>{t('section6.title')}</h3>
+            <p>{t('section6.paragraph1')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>6. Direct Marketing</h3>
-            <p>
-              We believe in the value of our Services and, as such, we may send you information about new products, services, and promotions. We will
-              not disclose your personal information to third parties for marketing purposes without your consent. You may opt out at any time if you
-              do not wish to receive commercial messages from us.
-            </p>
+            <h3>{t('section7.title')}</h3>
+            <p>{t('section7.paragraph1')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>7. Cookies</h3>
-            <p>
-              Cookies are small text files stored on your computer for a predefined period. We use cookies to identify specific machines and collect
-              aggregate information on site usage. This helps us adapt the Site to better suit user needs. For information on cookie settings, please
-              refer to your browser's manual.
-            </p>
+            <h3>{t('section8.title')}</h3>
+            <p>{t('section8.paragraph1')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>8. Links to Other Websites</h3>
-            <p>
-              Our website, Yes Job, may contain links to other websites for your convenience. This includes links to recruiters' websites and other
-              third-party sites relevant to job seeking and employment. Please note that we are not responsible for the privacy practices or the
-              content of these other websites. When you access third-party websites, such as recruiters' sites, through our platform, their own terms
-              and privacy policies will apply. We encourage our users to be aware when they leave our site and to read the privacy statements of each
-              and every website that collects personally identifiable information.
-            </p>
+            <h3>{t('section9.title')}</h3>
+            <p>{t('section9.paragraph1')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>9. Accessing and Correcting Your Personal Information</h3>
-            <p>
-              You may contact our Privacy Officer to access or correct your personal information. We will respond to requests within a reasonable time
-              frame.
-            </p>
+            <h3>{t('section10.title')}</h3>
+            <p>{t('section10.paragraph1')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>10. Overseas Transfers of Personal Information</h3>
-            <p>
-              From time to time, we may engage overseas recipients for services such as cloud-based storage solutions. By providing us with your
-              personal information, you consent to the storage of such information on overseas servers.
-            </p>
+            <h3>{t('section11.title')}</h3>
+            <p>{t('section11.paragraph1')}</p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>11. Resolving Personal Information Concerns</h3>
-            <p>
-              If you have any questions or complaints about this Privacy Policy or how we handle your personal information, please contact our Privacy
-              Officer. We take all complaints seriously and will respond within a reasonable period.
-            </p>
-          </div>
-          <div className='flex flex-col gap-2'>
-            <h3>12. Changes to This Privacy Policy</h3>
-            <p>
-              We reserve the right to change this Privacy Policy from time to time. The most current version of our Privacy Policy is available on our
-              website.
-            </p>
+            <h3>{t('section12.title')}</h3>
+            <p>{t('section12.paragraph1')}</p>
           </div>
         </div>
       </section>
