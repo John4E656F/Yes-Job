f414fce1347012f93f91f410a57671ba68c93bf8
John4E656F <jm49andres@gmail.com>
2023-12-21 17:33:04 +0100

    Finished TOS

--- app/[locale]/(home)/(policies)/terms/page.tsx
+++ app/[locale]/(home)/(policies)/terms/page.tsx
@@ -1,148 +1,128 @@
 import React from 'react';
+import { getTranslations } from 'next-intl/server';
 
-export default function TermsAndConditionsGeneralServicesPage() {
+export default async function TermsOfServicesPage() {
+  const t = await getTranslations('policies');
   return (
     <>
       <section className='w-full flex flex-col items-center px-10 justify-center py-10 bg-brand-lightbg'>
         <div className=' w-full flex flex-col justify-center text-center items-center '>
-          <p className='text-sm'>Current as of 17/12/2023</p>
-          <h1>Terms and Conditions</h1>
-          <p>By using the Yes Job website, you agree to be bound by these terms.</p>
+          <p className='text-sm'>{t('TOS.date')} 17/12/2023</p>
+          <h1>{t('TOS.title')}</h1>
+          <p>{t('TOS.subTitle')}</p>
         </div>
       </section>
       <section className='w-full flex flex-col items-center justify-center py-4 bg-white'>
         <div className='w-1/2 flex flex-col justify-center gap-5 mt-10 '>
           <div className='flex flex-col gap-2'>
-            <h2 className='text-left'>Terms of use for all users</h2>
+            <h2 className='text-left'>{t('TOS.introTitle')}</h2>
+            <p>{t('TOS.introTitle')}</p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3 className='text-left'>{t('TOS.detailsTitle')}</h3>
             <p>
-              By accessing Yes Job, you agree to comply with these terms of service, all applicable laws and regulations, and acknowledge that you are
-              responsible for compliance with any applicable local laws.
+              1. <b> {t('TOS.detailsBold')} </b>
+              {t('TOS.detailsText')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3 className='text-left'>Terms</h3>
+            <h3>{t('TOS.details1Title')}</h3>
             <p>
-              1. <b> Usage Restrictions: </b> If you do not agree with any of these terms, you are prohibited from using or accessing this site. The
-              materials contained in this website are protected by applicable copyright and trademark law.
-            </p>
-          </div>
-          <div className='flex flex-col gap-2'>
-            <h3>Use License</h3>
-            <p>
-              2. <b>Scope of License: </b> Permission is granted to temporarily download one copy of the materials (information or software) on Yes
-              Job's website for personal, non-commercial transitory viewing only. Under this license you may not:
+              2. <b>{t('TOS.details1Bold')} </b> {t('TOS.details1Text')}
             </p>
             <ul>
-              <li>Modify or copy the materials.</li>
-              <li>Use the materials for any commercial purpose, or for any public display.</li>
-              <li>Attempt to decompile or reverse engineer any software contained on Yes Job's website.</li>
-              <li>Remove any copyright or other proprietary notations from the materials.</li>
-              <li>Transfer the materials to another person or 'mirror' the materials on any other server.</li>
+              <li>{t('TOS.details1Li')}</li>
+              <li>{t('TOS.details1Li1')}</li>
+              <li>{t('TOS.details1Li2')}</li>
+              <li>{t('TOS.details1Li3')}</li>
+              <li>{t('TOS.details1Li4')}</li>
             </ul>
             <p>
-              3. <b> License Termination: </b> This license shall automatically terminate if you violate any of these restrictions and may be
-              terminated by Yes Job at any time. Upon terminating your viewing of these materials, you must destroy any downloaded materials in your
-              possession whether in electronic or printed format.
+              3. <b> {t('TOS.details1Title1')} </b> {t('TOS.details1Text1')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Disclaimer</h3>
+            <h3>{t('TOS.details2Title')}</h3>
             <p>
-              4. <b> As Is Basis: </b> The materials on Yes Job's website are provided on an 'as is' basis. Yes Job makes no warranties, expressed or
-              implied, and disclaims all other warranties including, without limitation, implied warranties of merchantability, fitness for a
-              particular purpose, or non-infringement of intellectual property or other violation of rights.
+              4. <b>{t('TOS.details2Bold')} </b> {t('TOS.details2Text')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Limitations</h3>
+            <h3>{t('TOS.details3Title')}</h3>
             <p>
-              5. <b> Liability Limitations: </b> In no event shall Yes Job or its suppliers be liable for any damages arising out of the use or
-              inability to use the materials on Yes Job's website, even if Yes Job or an authorized representative has been notified orally or in
-              writing of the possibility of such damage.
+              5. <b> {t('TOS.details3Bold')} </b> {t('TOS.details3Text')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Accuracy of Materials</h3>
+            <h3>{t('TOS.details4Title')}</h3>
             <p>
-              6. <b> Content Accuracy: </b> The materials appearing on Yes Job's website could include technical, typographical, or photographic
-              errors. Yes Job does not warrant that any of the materials on its website are accurate, complete, or current.
+              6. <b> {t('TOS.details4Bold')} </b> {t('TOS.details4Text')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Links</h3>
+            <h3>{t('TOS.details5Title')}</h3>
             <p>
-              7. <b> External Links: </b> Yes Job has not reviewed all sites linked to its website and is not responsible for the contents of any such
-              linked site. The inclusion of any link does not imply endorsement by Yes Job. Use of any such linked website is at the user's own risk.
+              7. <b> {t('TOS.details5Bold')} </b> {t('TOS.details5Text')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Modifications</h3>
+            <h3>{t('TOS.details6Title')}</h3>
             <p>
-              8. <b> Terms Revision: </b> Yes Job may revise these terms of service for its website at any time without notice. By using this website,
-              you agree to be bound by the current version of these terms of service.
+              8. <b> {t('TOS.details6Bold')} </b> {t('TOS.details6Text')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Governing Law</h3>
+            <h3>{t('TOS.details7Title')}</h3>
             <p>
-              9. <b> Jurisdiction: </b> These Terms are governed by the laws of Belgium, without regard to its conflict of law provisions. By using
-              Yes Job, you agree to submit to the exclusive jurisdiction of the courts located in Belgium for the resolution of any disputes arising
-              from or related to these Terms or your use of the Service.
+              9. <b> {t('TOS.details7Bold')} </b> {t('TOS.details7Text')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Services</h3>
+            <h3>{t('TOS.details8Text')}</h3>
             <p>
-              10. <b> Scope of Services: </b> These terms and conditions apply to the Yes Job website, communications, and services. Registered users
-              are “Members,” and unregistered users are “Visitors.” Members who have signed up for employer accounts are “Employers,” while job
-              seekers are “Job Seekers.” Visitors or Members who apply for a job using the Site are “Candidates.”
+              10. <b> {t('TOS.details8Bold')} </b> {t('TOS.details8Text')}
             </p>
             <p>
-              11. <b> Site Ownership: </b> The Site is owned and operated by Yes Job (defined as “Yes Job”, “we”, “us” and “our”). Your continued use
-              of this Site signifies your acceptance of these Terms.
+              11. <b> {t('TOS.details8Bold1')} </b> {t('TOS.details8Text1')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Variation of Terms</h3>
+            <h3>{t('TOS.details9Title')}</h3>
             <p>
-              12. <b> Terms Update: </b> We may update these Terms at any time, with notice published on the Site.
+              12. <b> {t('TOS.details9Bold')} </b> {t('TOS.details9Text')}
             </p>
             <p>
-              13.<b> Notification of Changes: </b> Changes to the Terms will be communicated with reasonable notice, especially for changes materially
-              detrimental to you.
-            </p>
-          </div>
-          <div className='flex flex-col gap-2'>
-            <h3>Intellectual Property Rights</h3>
-            <p>
-              14.<b> Ownership:</b> Yes Job retains all intellectual property rights in the services provided to you.
+              13.<b> {t('TOS.details9Bold1')} </b> {t('TOS.details9Text1')}
             </p>
           </div>
           <div className='flex flex-col gap-2'>
-            <h3>Limitation of Liability and Disclaimer</h3>
+            <h3>{t('TOS.details10Title')}</h3>
             <p>
-              15. <b> Service Warranty: </b> Yes Job provides no warranty that services available through the Site will be uninterrupted or
-              error-free. Our liability is limited to the maximum extent permitted by law.
+              14.<b> {t('TOS.details10Bold')} </b> {t('TOS.details10Text')}
+            </p>
+          </div>
+          <div className='flex flex-col gap-2'>
+            <h3>{t('TOS.details11Title')}</h3>
+            <p>
+              15. <b> S{t('TOS.details11Bold')} </b> {t('TOS.details11Text')}
             </p>
             <p>
-              16. <b> Liability Cap: </b> Liability for breach of any implied warranty, which cannot be excluded, is limited to the supply of the
-              services again or the cost of having the services supplied again.
+              16. <b> {t('TOS.details11Bold1')} </b> {t('TOS.details11Text1')}
             </p>
             <p>
-              17. <b> Disclaimer Extent: </b> Our liability disclaimer applies to direct, indirect, and consequential damages.
+              17. <b> {t('TOS.details11Bold2')} </b> {t('TOS.details11Text2')}
             </p>
             <p>
-              18. <b> Content Responsibility: </b> Yes Job accepts no responsibility for errors in your advertisements on our Site.
+              18. <b> {t('TOS.details11Bold3')} </b> {t('TOS.details11Text3')}
             </p>
             <p>
-              19. <b> Service Effort: </b> Yes Job will make reasonable efforts to publish advertisements promptly.
+              19. <b> {t('TOS.details11Bold4')} </b> {t('TOS.details11Text4')}
             </p>
             <p>
-              20. <b> Data Security: </b> Yes Job does not guarantee files available for download through the Site will be free of harmful elements.
+              20. <b> {t('TOS.details11Bold5')} </b> {t('TOS.details11Text5')}
             </p>
             <p>
-              21. <b> Role as a Medium: </b> Yes Job acts as a medium for employment opportunities and does not vet Candidates or their
-              representations.
+              21. <b> {t('TOS.details11Bold6')} </b> {t('TOS.details11Text6')}
             </p>
           </div>
         </div>
