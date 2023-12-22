e833d5bfa47dbe044b6dfbcc43840acb95f0cbb4
John4E656F <jm49andres@gmail.com>
2023-12-22 16:58:10 +0100

    Build storage helpers

---
+++ lib/storageHelper.ts
@@ -0,0 +1,11 @@
+'use client';
+
+export function getLocalStorage(key: string, defaultValue: any) {
+  const stickyValue = localStorage.getItem(key);
+
+  return stickyValue !== null && stickyValue !== 'undefined' ? JSON.parse(stickyValue) : defaultValue;
+}
+
+export function setLocalStorage(key: string, value: any) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
